e2c6bdca710a6d7033a73d48d071ff88171a1b92
Mohit Sudhakar <mohit.sudhakar@gmail.com>
2018-02-02 13:06:03 +0530

    changed url

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ console.log("App listening on port " + port);
 
 // routes
 
-app.all("/api/*", function (req, res, next) {
+app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
